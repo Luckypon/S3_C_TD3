e61f761f3da4babc27439f5f032e83f75587ef2b
Luckypon <marion.vila.25@live.fr>
2016-10-12 21:53:51 +0200

    Modification + ajout de recherche, supprimer, menu, tests

--- td3.cpp
+++ td3.cpp
@@ -1,4 +1,4 @@
-// test
+// Separer les éléments
 
 #include <iostream>
 #include <vector>
@@ -35,6 +35,14 @@ class Adress
 		
 		void setCountry (string c) {
 			country = c;
+		}
+
+		void setAll(unsigned int n, string s, unsigned int z, string ci, string co){
+			setNumber(n);
+			setStreet(s);
+			setZipCode(z);
+			setCity(ci);
+			setCountry(co);
 		}
 
 
@@ -117,6 +125,7 @@ public:
 		printAge();
 		printHeight();
 		printAdress();
+		cout << endl;
 	}
 
 	string getName() {
@@ -146,6 +155,12 @@ public:
 	}
 	void setAdress(Adress a){
 		adress = a;
+	}
+	void setAll(string n, unsigned int a, unsigned int h, Adress ad){
+		setName(n);
+		setAge(a);
+		setHeight(h);
+		setAdress(ad);
 	}
 
 	void addInfos(){
@@ -177,37 +192,177 @@ class AdressBook{
 	public:
 		AdressBook(){}
 
+		void addContact(Person p){
+			adressBook.push_back(p);
+			cout << "Vous venez de rentrer le contact "  << endl;
+			p.printAll();	
+			cout << endl;
+		}
+
 		void addContact(){
 			Person p;
-			
+			cout << "----------------------- NOUVEAU CONTACT ----------------------"  << endl << endl;
 			p.addInfos();
 
 			adressBook.push_back(p);
 
-			cout << "Vous venez de rentrer le contact "  << endl;	
+			cout << "Vous venez de rentrer le contact "  << endl;
+			p.printAll();	
+			cout << "---------------------------------------------"  << endl << endl;
 		}
-		void removeContact(){
 
-		}
-		void print(){
+		Person searchContact(string _name){
+			cout << "----------------------- RECHERCHE ----------------------"  << endl << endl;
 			vector<int>::size_type sz = adressBook.size();
  			for (unsigned i=0; i<sz; i++) {
-				adressBook[i].printAll();
-				cout << endl;
+ 				if(_name == adressBook[i].getName()){
+ 					cout << "Ce contact existe :"  << endl;
+ 					adressBook[i].printAll();
+ 					cout << "---------------------------------------------"  << endl << endl;
+ 					return adressBook[i];	
+ 				}
+ 			}
+ 			Person p;
+ 			cout << "Ce contact n'existe pas"  << endl;
+ 			cout << "---------------------------------------------"  << endl << endl;
+			return p;
+		}
+
+		int searchContactPlace(Person p){
+			vector<int>::size_type sz = adressBook.size();
+ 			for (unsigned i=0; i<sz; i++) {
+ 				if(p.getName() == adressBook[i].getName()){ //a modifier ?? deux personnes peuvent avoir le même nom..
+ 					return i;	
+ 				}
+ 			}
+ 			return -1;
+		}
+
+		void editContact(Person p){ // a faire
+
+		}
+
+		void removeContact(Person p){
+			cout << "----------------------- SUPPRESSION D'UN CONTACT ----------------------"  << endl << endl;
+			int position = searchContactPlace(p);
+			if(position == -1){
+				cout << "Ce contact n'existe pas, et ne peut donc pas être supprimé" << endl << endl;
+			}else{
+				cout << "Vous allez supprimer ce contact" << endl;
+				p.printAll();
+				adressBook.erase(adressBook.begin()+position);
+				cout << "Ce contact a été supprimé" << endl << endl;
+				cout << "-------------------------------------------------" << endl << endl;
 			}
+			
+		}
+
+		void exportToFile(){ //a faire
+
+		}
+
+		void print(){
+			vector<int>::size_type sz = adressBook.size();
+			int nbTest = 0, limite = 10;
+			string junk;
+			cout << "----------------------- LISTE DES CONTACTS : 10max affiché ----------------------"  << endl << endl;
+ 			for (unsigned i=0; i<sz; i++) {
+ 				if(nbTest < limite){
+ 					nbTest ++;
+ 					cout << "Contact : "  << i << endl;
+					adressBook[i].printAll();
+					cout << endl;
+ 				}else{
+ 					cout << "Pressez un bouton pour voir les autres contacts "  << endl << endl;
+ 					cin >> junk;
+ 					nbTest = 0;
+ 					cout << endl;
+ 				}
+ 				
+			}
+			cout << "-------------------------------------------------" << endl << endl;
 		}
 
 };
 
 
+void testContacts(AdressBook *book){
+	Person a,b,c,d,e,f,g,h,i,j,k,l,m,n;
+	Adress _a,_b,_c,_d,_e,_f,_g,_h,_i,_j,_k,_l,_m,_n;
+	_a.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	a.setAll("JeanMiche1", 23, 180, _a);
+	_b.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	b.setAll("JeanMiche2", 23, 180, _b);
+	_c.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	c.setAll("JeanMiche3", 23, 180, _c);
+	_d.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	d.setAll("JeanMiche4", 23, 180, _d);
+	_e.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	e.setAll("JeanMiche5", 23, 180, _e);
+	_f.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	f.setAll("JeanMiche6", 23, 180, _f);
+	_g.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	g.setAll("JeanMiche7", 23, 180, _g);
+	_h.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	h.setAll("JeanMiche8", 23, 180, _h);
+	_i.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	i.setAll("JeanMiche9", 23, 180, _i);
+	_j.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	j.setAll("JeanMiche10", 23, 180, _j);
+	_k.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	k.setAll("JeanMiche11", 23, 180, _k);
+	_l.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	l.setAll("JeanMiche12", 23, 180, _l);
+	_m.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	m.setAll("JeanMiche13", 23, 180, _m);
+	_n.setAll(20, "Rue des fleurs", 77420, "Paris", "Canada");
+	n.setAll("JeanMiche14", 23, 180, _n);
 
+	book->addContact(a);
+	book->addContact(b);
+	book->addContact(c);
+	book->addContact(d);
+	book->addContact(e);
+	book->addContact(f);
+	book->addContact(g);
+	book->addContact(h);
+	book->addContact(i);
+	book->addContact(j);
+	book->addContact(k);
+	book->addContact(l);
+	book->addContact(m);
+	book->addContact(n);
+
+}
+
+void menu(){ // a faire en pratique
+	cout << "----------------------MENU PRINCIPAL--------------------------" << endl;
+	cout << "1) Affichez les contacts \t 2) Cherchez un contact \t 3) Ajoutez un contact" <<
+	"\t 4) Supprimez un contact \t 5) Editez un contact \t Exportez" << endl;
+}
 
 
 
 
 int main () {
 	AdressBook book;
-	book.addContact();
+	Person test;
+	Adress _test;
+
+	_test.setAll(20, "Rue des pigeons", 77420, "Paris", "France");
+	test.setAll("Eustache", 23, 180, _test);
+	book.addContact(test);
+
+	testContacts(&book);
+
+
+
+	book.print();
+
+	book.searchContact("JeanMiche14");
+
+	book.removeContact(test);
+
 	book.print();
 
 	return 0;
